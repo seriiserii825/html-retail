7ac1c257e661b9515bed04aa5a2080ee84a3d22e
serii <seriiburduja@gmail.com>
2022-06-22 17:32:51 +0300

    feat: footer-mobile

--- src/assets/jquery/custom-jquery.js
+++ src/assets/jquery/custom-jquery.js
@@ -1,12 +1,23 @@
-import $ from 'jquery';
-
-let customJquery = function () {
-	$(function () {
-		$('.open-list').on('click', (e) => {
-			e.preventDefault();
-			$('.sub-menu').slideToggle();
-		});
-	});
-};
-
-export default customJquery;
+jQuery(document).ready(function ($) {
+    const mainMenu = $('#js-main-menu');
+    let closeMenu = function () {
+    	$('#js-sandwitch-wrap').on('click', function () {
+    		if (mainMenu.hasClass('fixed')) {
+    			mainMenu.removeClass('fixed');
+    			$('body').css({
+    				overflow: '',
+    				paddingRight: 'initial'
+    			});
+    		} else {
+    			$('body').css({
+    				overflow: 'hidden',
+    				paddingRight: '12px'
+    			});
+    			mainMenu.addClass('fixed');
+    		}
+    		$(this).toggleClass('sandwitch--active');
+    		$(this).closest('.sandwitch-wrapper').toggleClass('sandwitch--active');
+    	});
+    };
+    closeMenu();
+});
